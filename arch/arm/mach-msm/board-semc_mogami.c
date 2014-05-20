7ffff8e20cd5b36fcb167fa58e6d9e8b18174b53
Vishal Dubey <yzvishal.vd@gmail.com>
2014-05-20 12:29:03 -0400

    Get that RAM up like never before

--- arch/arm/mach-msm/board-semc_mogami.c
+++ arch/arm/mach-msm/board-semc_mogami.c
@@ -208,14 +208,14 @@
 #endif
 
 #ifdef CONFIG_FB_MSM_HDPI
-#define MSM_PMEM_SF_SIZE  0x1600000
+#define MSM_PMEM_SF_SIZE  0x0100000
 #ifdef CONFIG_FB_MSM_TRIPLE_BUFFER
 #define MSM_FB_PRIM_BUF_SIZE   (864 * 480 * 4 * 3) /* 4bpp * 3 Pages */
 #else
 #define MSM_FB_PRIM_BUF_SIZE   (864 * 480 * 4 * 2) /* 4bpp * 2 Pages */
 #endif
 #else
-#define MSM_PMEM_SF_SIZE  0x1600000
+#define MSM_PMEM_SF_SIZE  0x0100000
 #ifdef CONFIG_FB_MSM_TRIPLE_BUFFER
 #define MSM_FB_PRIM_BUF_SIZE   (480 * 320 * 4 * 3) /* 4bpp * 3 Pages */
 #else
